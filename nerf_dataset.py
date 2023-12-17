cf8a6ae2607dd816643d71e94e92a269ec79bae1
Sourish Kundu <sourishkundu07@gmail.com>
2023-12-17 14:20:55 -0800

    Working on optimizations

---
+++ nerf_dataset.py
@@ -0,0 +1,31 @@
+import torch
+from torch.utils.data import Dataset
+from torchvision import transforms
+import os
+import json
+from PIL import Image
+
+class LegoDataset(Dataset):
+    def __init__(self, split="train"):
+        self.data_directory = "lego"
+        self.split = split
+        self.transform = transforms.Compose([
+            transforms.Resize(64),
+            transforms.ToTensor()
+        ])
+
+        with open(os.path.join(self.data_directory, f'transforms_{split}.json'), "r", encoding="utf-8") as f:
+            self.data = json.load(f)["frames"]
+
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, index):
+        index = 0
+        img_name = os.path.join(self.data_directory, self.split, f"r_{index}.png")
+        img = Image.open(img_name).convert('RGB')  # Convert to RGB if alpha channel is not needed
+
+        if self.transform:
+            img = self.transform(img)
+
+        return img, torch.tensor(self.data[index]["transform_matrix"], dtype=torch.float32)
--- nerf_utils.py
+++ nerf_utils.py
@@ -1,24 +1,27 @@
 import numpy as np
 import json
+import torch
 from PIL import Image
 
 
-def read_in_data(idx=None, split='train'):
+def read_in_dataset(idx=None, split='train'):
     assert split in ['train', 'val', 'test']
-    with open(f'lego/transforms_{train}.json', "r") as f:
+    with open(f'lego/transforms_{split}.json', "r") as f:
         data = json.load(f)
+
+
 
     if idx is None:
         return np.array([data['frames'][i]['transform_matrix'] for i in range(len(data['frames']))])
     else:
         return np.array(data['frames'][idx]['transform_matrix'])
-    
+
+
 def read_in_image(idx=0, split='train'):
-    img = Image.open(f'lego/{split}/r_88.png')
+    img = Image.open(f'lego/{split}/r_{idx}.png')
 
     # convert img to tensor
     img = np.array(img)
-    height, weight, _ = img.shape
 
     # set all transparent pixels to white
     # img[..., 3] == 0 is the alpha channel
@@ -26,51 +29,50 @@ def read_in_image(idx=0, split='train'):
     # the == 0 returns a boolean array, which we use to index into the image
     img[img[..., 3] == 0, :3] = 255
 
-    return img
+    return torch.tensor(img, device="cuda", dtype=torch.float32)
     
 
-
-def get_rays(width, height, cam_2_world):
+def get_rays(cam_2_world, width=800, height=800):
+    device = cam_2_world.device
+    
     aspect_ratio = width / height
-    focal_length = 1
+    focal_length = 3
     image_height = 2
     image_width = aspect_ratio * image_height
 
-    # Creating the top left and bottom right corners of the image
-    top_left = np.array([-image_width / 2, image_height / 2, -focal_length])
-    bottom_right = np.array([image_width / 2, -image_height / 2, -focal_length])
-
     # Creating the grid of points
-    i, j = np.meshgrid(
+    i, j = torch.meshgrid(
         # top_left[0] is x coordinate of top left corner (left bound)
         # bottom_right[0] is x coordinate of bottom right corner (right bound)
-        np.linspace(top_left[0], bottom_right[0], width) + (1 / (2 * width)),
+        torch.linspace(-image_width / 2, image_width / 2, width, device=device) + (1 / (2 * width)),
         # top_left[1] is y coordinate of top left corner (top bound)
         # bottom_right[1] is y coordinate of bottom right corner (bottom bound)
-        np.linspace(top_left[1], bottom_right[1], height) + (1 / (2 * height)),
+        torch.linspace(image_height / 2, -image_height / 2, height, device=device) + (1 / (2 * height)),
         indexing='ij'
     )
-    i, j = i.T, j.T
-    directions = np.stack([i, j, -np.ones_like(i)]).reshape((3, -1))
-
-    rays_d = cam_2_world[:3, :3] @ directions
-    rays_o = np.broadcast_to(cam_2_world[:3, [-1]], rays_d.shape)
-
+    
+    directions = torch.stack([i, j, -torch.ones_like(i, device=device) * focal_length])
+    # normalize the directions
+    directions /= torch.norm(directions, dim=0, keepdim=True)
+    rays_d = cam_2_world[:3, :3] @ directions.reshape(3, -1)
+    rays_d = rays_d.reshape(3, height, width)
+    rays_o = torch.broadcast_to(cam_2_world[:3, -1], (height, width, 3)).permute(2, 0, 1)
     return rays_o, rays_d
 
-def stratified_sampling():
-    # Stratified sampling
-    N = 192
-    t_near = 0
-    t_far = 6
 
-    u = np.linspace(t_near, t_far, N + 1)[:-1]
+def stratified_sampling(device="cuda"):
+    # Stratified sampling
+    N = 151
+    t_near = 1
+    t_far = 7
+
+    u = torch.linspace(t_near, t_far, N + 1, device=device)[:-1]
     width_of_bin = (t_far - t_near) / N
 
     # generate N random samples from a uniform distribution
-    t = np.rand(N) * width_of_bin + u
+    t = torch.rand(N, dtype=torch.float32, device=device) * width_of_bin + u
     d = t[1:] - t[:-1]
 
-    return d
+    return t[1:], d
 
 
