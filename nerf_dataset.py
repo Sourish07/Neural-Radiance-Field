5b059b115c9fd3e4f290b224ba52c58ccc0d3115
Sourish Kundu <sourishkundu07@gmail.com>
2023-12-17 14:57:44 -0800

    Refactor LegoDataset initialization and add new_image_size parameter

--- nerf_dataset.py
+++ nerf_dataset.py
@@ -6,32 +6,27 @@ import json
 from PIL import Image
 
 class LegoDataset(Dataset):
-    def __init__(self, split="train"):
+    def __init__(self, split="train", new_image_size=800):
         self.data_directory = "lego"
         self.split = split
+        self.new_image_size = new_image_size
+
         self.transform = transforms.Compose([
-            transforms.Resize(64),
+            transforms.Resize(self.new_image_size),
             transforms.ToTensor()
         ])
 
         with open(os.path.join(self.data_directory, f'transforms_{split}.json'), "r", encoding="utf-8") as f:
             self.data = json.load(f)["frames"]
 
-        self.first_point = None
-
     def __len__(self):
         return len(self.data)
     
     def __getitem__(self, index):
-        index = 0
-        if self.first_point:
-            return self.first_point
         img_name = os.path.join(self.data_directory, self.split, f"r_{index}.png")
         img = Image.open(img_name).convert('RGB')  # Convert to RGB if alpha channel is not needed
 
         if self.transform:
             img = self.transform(img)
-
-        self.first_point = (img, torch.tensor(self.data[index]["transform_matrix"], dtype=torch.float32))
 
         return img, torch.tensor(self.data[index]["transform_matrix"], dtype=torch.float32)
